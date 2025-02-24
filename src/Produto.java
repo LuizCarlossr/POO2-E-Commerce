b0ee82e042879c3848aade3d2882cec3314d14ce
Mattheus <mattheustib@gmail.com>
2025-02-24 19:51:29 -0300

    Classe Produto

---
+++ src/Produto.java
@@ -0,0 +1,48 @@
+public class Produto {
+    private int id;          // Identificador único do produto
+    private String nome;     // Nome do produto
+    private double preco;    // Preço do produto
+
+    // Construtor para inicializar um produto
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    // Método toString para representação textual do produto
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
